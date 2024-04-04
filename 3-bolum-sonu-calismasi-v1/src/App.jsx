7c2b04b803eb2897e6176ecc01b066ca82ac3657
Zeki Burak Özden <zburakozden@gmail.com>
2024-04-04 17:36:44 +0300

    ilk bolum sonu calismamizi ekledik: degisen mini gorseller.

---
+++ 3-bolum-sonu-calismasi-v1/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+function App() {
+  const pictureIds = [200, 201, 202, 203, 204]
+  const [imageId, setImageId] = useState(pictureIds[0])
+
+  const getLoremPicsumImg = (id, width=800, height=450) => `https://picsum.photos/id/${id}/${width}/${height}`
+
+  function handleClick(id) {
+    setImageId(id)
+  }
+  
+  return (
+    <main className='container'>
+      <hgroup>
+        <h2>Mini Gorsel Uygulamasi v1</h2>
+        <h3>Gorsel:</h3>
+      </hgroup>
+      <img src={getLoremPicsumImg(imageId)} alt="" style={{width: "100%"}} />
+      <div className="grid" style={{marginTop: "30px"}}>
+        {
+          pictureIds.map((item, index) => <div key={index}>
+            <img 
+              onClick={() => handleClick(item)}
+              src={getLoremPicsumImg(item, 100, 100)} 
+              alt="" 
+            />
+          </div>)
+        }
+      </div>
+    </main>
+  )
+}
+
+/* 
+5 adet gorsel render etmek icin kullandik.
+  {
+    [...Array(5)].map((item, index) => <div key={index}>
+      <img 
+        onClick={() => handleClick(200 + index)}
+        src={getLoremPicsumImg((200 + index), 100, 100)} 
+        alt="" 
+      />
+    </div>)
+  }
+*/ 
+
+export default App
