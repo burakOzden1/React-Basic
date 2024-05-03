824795a432b4e599e75dde9d47fdbd9ed4d4224c
Zeki Burak Özden <zburakozden@gmail.com>
2024-05-03 22:47:31 +0300

    Kategoriler eklendi ve urunler sayfasi duzenlendi.

--- 6-react-router-dom-ile-calismak/src/SiteRoutes.jsx
+++ 6-react-router-dom-ile-calismak/src/SiteRoutes.jsx
@@ -3,15 +3,19 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import PageNotFound from './pages/PageNotFound'
+import { ProductLayout, Products } from './products'
 // import Product from './products/Product'
 
 export default function SiteRoutes() {
     return (
         <Routes>
           <Route path='/' element={<Home/>}/>
-          {/* <Route path='/urunler' element={<Product/>}/> */}
           <Route path='/about' element={<About/>}/>
           <Route path='/contact' element={<Contact/>}/>
+          <Route path='/products' element={<ProductLayout/>}>
+          <Route index={true} element={<Products/>}/>
+
+          </Route>
           <Route path='*' element={<PageNotFound/>}/> {/* eger yukaridaki rootlar disinda bir root girilirse PageNotFound sayfasina yonlendirir.*/}
         </Routes>
     )
